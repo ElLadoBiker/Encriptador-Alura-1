70e93d95133e6613fd3b64487456160ef71567a4
ElLadoBiker <elladobiker@gmail.com>
2023-01-21 23:58:45 -0600

    Actualizacion js

--- js/script.js
+++ js/script.js
@@ -10,13 +10,29 @@ function btnEncriptar(){
 
 function encriptar(stringEncriptada){
   let matrizCodigo =[["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-  stringEncriptada = stringEncriptada.toLowerCase()
-  
+  stringEncriptada = stringEncriptada.toLowerCase()  
   for(let i = 0; i < matrizCodigo.length; i++){
-    if(stringEncriptada.includes(matrizCodigo[i][0])){
-        stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
-    }
-
+     stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
   }
   return stringEncriptada
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo =[["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+    
+    for(let i = 0; i < matrizCodigo.length; i++){
+        stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+    }
+    return stringDesencriptada
+  }
+
+  function btncopiar(){
+    navigator.clipboard.writeText(mensaje.value);
 }
